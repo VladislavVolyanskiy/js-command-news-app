416e9719b521b6326362ffd45bcfe8fdc4b2871c
Rationalist <vadys.vern@gmail.com>
2023-03-04 22:03:36 +0200

    Refactored markup

--- src/js/news-card.js
+++ src/js/news-card.js
@@ -4,38 +4,48 @@ export default function createmarkup({
   articleTitle,
   shortDescription,
   urlOriginalArticle,
-  imgUrl, articleId
+  imgUrl,
+  articleId,
 }) {
-  // const cardItems = data.map(({ publishedDate, sectionName, articleTitle, urlOriginalArticle, imgUrl }) => {
+//   const cardItems = data.map(({
+//     publishedDate,
+//     sectionName,
+//     articleTitle,
+//     urlOriginalArticle,
+//     imgUrl
+//     }) => {
+
   let cardItems = '';
   return (cardItems = `
-        <div class="card__search" id="${articleId}">
-            <div>
-            <img class="card__image" src="${imgUrl}" alt="${articleTitle}" />
-            <p class="card__job"><span>card job</span></p>
-            <button class="card__favorite">
-                <span>Add to favorite</span>
-            </button>
-            </div>
-            <div class="card__header">
-            <h2>
-                <span>${sectionName}</span>
-            </h2>
-            </div>
-            <div>
-            <p class="card__text">
-                <span>${articleTitle}</span>
-            </p>
-            </div>
-            
-            <div>
-            <p class="card__data">
-                <span>${publishedDate}</span>
-            </p>
-            <a href="${urlOriginalArticle}" class="card__more" rel="stylesheet">
-                <span>Read more</span>
-            </a>
-            </div>
+      <article class="card" id="${articleId}">
+        <div class="card__img-container">
+          <img class="card__img" src="${imgUrl}" alt="${articleTitle}">
+          <p class="card__section-name">
+            ${sectionName}
+          </p>
+          <button class="card__btn" type="button">
+            Add to favorite
+            <svg class="card__btn-icon-unclicked">
+              <use href="./images/symbol-defs.svg#icon-icon-heart"></use>
+            </svg>
+          </button>
         </div>
+        <h2 class="card__title">
+          ${articleTitle}
+        </h2>
+        <p class="card__text">
+          ${shortDescription}
+        </p>
+        <div class"card__bottom>
+          <p class="card__date">
+            ${publishedDate}
+          </p>
+          <a class="card__read-more-search"
+            href="${urlOriginalArticle}"
+          >
+            Read more
+          </a>
+        </div>
+      </article>
     `);
-}
+  };
