d710e19425f7c69b1e817d3f50bea3658b8220bd
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-03-05 15:40:39 +0100

    fixed

--- src/js/news-card.js
+++ src/js/news-card.js
@@ -7,16 +7,15 @@ export default function createmarkup({
   imgUrl,
   articleId,
 }) {
-//   const cardItems = data.map(({
-//     publishedDate,
-//     sectionName,
-//     articleTitle,
-//     urlOriginalArticle,
-//     imgUrl
-//     }) => {
+  let textLength = shortDescription;
+      if (shortDescription.length > 110) {
+          const spaceIndex = shortDescription.indexOf(" ", 110);
+          if (spaceIndex > 0) {
+              textLength = shortDescription.slice(0, spaceIndex) + "...";
+          }
+      }
 
-  let cardItems = '';
-  return (cardItems = `
+  return `
       <article class="card" id="${articleId}">
         <div class="card__img-container">
           <img class="card__img" src="${imgUrl}" alt="${articleTitle}">
@@ -30,7 +29,7 @@ export default function createmarkup({
         <h2 class="card__title">
           ${articleTitle}
         </h2>
-        <p class="card__text">${shortDescription}</p>
+        <p class="card__text">${textLength}</p>
         <div class="card__bottom">
           <span class="card__date">
             ${publishedDate}
@@ -42,5 +41,57 @@ export default function createmarkup({
           </a>
         </div>
       </article>
-    `);
-  };
+    `;
+};
+  
+
+// export default function createmarkup({
+//   publishedDate,
+//   sectionName,
+//   articleTitle,
+//   shortDescription,
+//   urlOriginalArticle,
+//   imgUrl,
+//   articleId
+// }) {
+//   // const cardItems = data.map(({ publishedDate, sectionName, articleTitle, urlOriginalArticle, imgUrl }) => {
+//     let textLength = shortDescription;
+//     if (shortDescription.length > 110) {
+//         const spaceIndex = shortDescription.indexOf(" ", 110);
+//         if (spaceIndex > 0) {
+//             textLength = shortDescription.slice(0, spaceIndex) + "...";
+//         }
+//     }
+//     console.log("publishedDate", publishedDate);
+//   return (`
+//         <div class="card__search">
+//             <div>
+//             <img class="card__image" src="${imgUrl}" alt="${articleTitle}" />
+//             <p class="card__job"><span>${sectionName} searchig</span></p>
+//             <button class="card__favorite" data-id=${articleId}>
+//                 <span>Add to favorite</span>
+//             </button>
+//             </div>
+//             <div class="card__header">
+//             <h2>
+//                 <span>${articleTitle}</span>
+//             </h2>
+//             </div>
+//             <div>
+//             <p class="card__text">
+//                 <span>${textLength}</span>
+//             </p>
+//             </div>
+            
+//             <div>
+//             <p class="card__data">
+//                 <span>${publishedDate}</span>
+//             </p>
+//             <a href="${urlOriginalArticle}" class="card__more" rel="stylesheet" target="_blank">
+//                 <span>Read more</span>
+//             </a>
+//             </div>
+//         </div>
+//     `);
+// }
+
