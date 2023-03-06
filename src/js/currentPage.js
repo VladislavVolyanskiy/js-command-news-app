df20197fdafb7c40d57ee8b47edfabc96952f959
Sforka <warlock20115@gmail.com>
2023-03-06 20:25:09 +0200

    1
    1

--- src/js/currentPage.js
+++ src/js/currentPage.js
@@ -3,9 +3,9 @@
 let currentPage = document.querySelector('body').getAttribute('data-current-page'); 
 // console.log(currentPage)
 if (currentPage === "index") {
-  document.querySelector('.nav_link[href="/index.html"]').classList.add('nav_link--current');  
+  document.querySelector('.nav_link[href="./index.html"]').classList.add('nav_link--current');  
 } else if (currentPage === "favorite") {
-  document.querySelector('.nav_link[href="/favorite.html"]').classList.add('nav_link--current');  
+  document.querySelector('.nav_link[href="./favorite.html"]').classList.add('nav_link--current');  
 } else if (currentPage === "read") {
-  document.querySelector('.nav_link[href="/read.html"]').classList.add('nav_link--current');  
+  document.querySelector('.nav_link[href="./read.html"]').classList.add('nav_link--current');  
 }
